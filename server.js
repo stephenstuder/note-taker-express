c2ed6e49d788259916b1b1b7e3ccc70ad13494ed
stephen studer <stephenkstuder@gmail.com>
2020-05-08 07:52:16 -0700

    added needed express code and listener

--- server.js
+++ server.js
@@ -1,8 +1,15 @@
 const express = require("express");
 
 const app = express();
-const PORT = process.argv || 8080;
+const PORT = process.argv.PORT || 8000;
 
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 
+
+
+
+
+app.listen(PORT, () => {
+  console.log(`Server is listening on http://localhost:/${PORT}`);
+})
